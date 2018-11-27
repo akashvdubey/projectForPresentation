7b7f00f5f0224388b71164095eca3b2b2686608b
akashvdubey <akashvdubey@users.noreply.github.com>
2018-11-27 23:30:35 +0530

    the changes when was about to present date 27 november

--- signup.php
+++ signup.php
@@ -1,58 +1,27 @@
 <?php
-$servername = "localhost";
-$username = "";
-$password = "";
-
-// Create connection
-$conn = mysqli_connect($servername, $username, $password);
-
-// Check connection
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-}
-echo "Connected successfully";
-mysqli_select_db($conn,"laundry");
-if(!empty($_GET))
+if(isset($_POST["b"]))
 {
-     $output = '';
-     $message = '';
-     $fname = mysqli_real_escape_string($conn, $_GET["firstName"]);
-     $lname = mysqli_real_escape_string($conn, $_GET["secondName"]);
-     $gender = mysqli_real_escape_string($conn, $_GET["gender"]);
-     $phone = mysqli_real_escape_string($conn, $_GET["MoNumber"]);
-     $dob = mysqli_real_escape_string($conn, $_GET["dob"]);
-     $mail = mysqli_real_escape_string($conn, $_GET["eMail"]);
 
-     $sql="INSERT INTO details(fname,lname,sex,phone,email,dob)
-           VALUES('$fname', '$lname', '$gender', '$phone','$mail', '$dob');";
+ $fname=$_POST['firstName'];
+ $lname=$_POST['secondName'];
+ $phone=$_POST['MoNumber'];
+ /*$gender=$_POST['gender'];*/
+ $mail=$_POST['eMail'];
+ $dob=$_POST['dob'];
+
+
+  $con=mysqli_connect("localhost","","");
+  if (!$con)
+  {
+    die('Could not connect: ' . mysqli_error());
+  }
+  mysqli_select_db( $con,"laundry");
+  $sql="insert into details value('$fname', '$lname', '$phone','$mail', '$dob');";
+  mysqli_query($con,$sql);
+  if(mysqli_affected_rows($con)>0)
+    echo "Record Inserted";
+
+  else
+    echo "Record Not Inserted";
 }
-if(mysqli_query($conn, $sql))
-    {
-      echo "true";
-         $output .= '<label class="text-success">' . $message . '</label>';
-         $select_query = "SELECT * FROM details ORDER BY fname DESC";
-         $result = mysqli_query($conn, $select_query);
-         $output .= '
-              <table class="table table-bordered">
-                   <tr>
-                        <th width="70%">Employee Name</th>
-                        <th width="15%">Edit</th>
-                        <th width="15%">View</th>
-                   </tr>
-         ';
-         while($row = mysqli_fetch_array($result))
-         {
-              $output .= '
-                   <tr>
-                        <td>' . $row["fname"] . '</td>
-                        <td><input type="button" name="edit" value="Edit" id="'.$row["email"] .'" class="btn btn-info btn-xs edit_data" /></td>
-                        <td><input type="button" name="view" value="view" id="' . $row["lname"] . '" class="btn btn-info btn-xs view_data" /></td>
-                   </tr>
-              ';
-         }
-         $output .= '</table>';
-    }
-    echo $output;
-      echo"error";
-
 ?>
