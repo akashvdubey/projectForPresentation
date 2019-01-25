8ae336c1494467d64ed54776862ce010766765a8
akashvdubey <akashvdubey@users.noreply.github.com>
2019-01-25 21:34:33 +0530

    done

--- assignment1.py
+++ assignment1.py
@@ -1,11 +1,22 @@
 x=list(input("enter the master word"))
-for i in x:
-    print(i)
+r=list()
+#for i in x:
+#    print(i)
 n=len(x)
-for k in range(0,n-1):
+for k in range(1,n):
+    c=0
+    #print("                           the size of empty is{}".format(k))
     for j in range(0,n-k+1):
+        #print("the {}th iteration from {} to {}".format(j,j,n-k))
         temp=x.copy()
         for p in range(j,j+k):
-            temp.remove(temp[p])
+            #print("removing from {} to {} location now on {}".format(j,j+k,p))
+            temp[p]='?'
+            c+=1
+        for v in range(c):
+            temp.remove('?')
+        c=0
         finalstring="".join(b for b in temp)
-        print(finalstring)
+        r.insert(0,finalstring)
+        r.sort()
+        print(r)
