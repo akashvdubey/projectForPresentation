7a99e66e7f1c1e61ca7e3e665d3a6042f7d3a06d
akashvdubey <akashvdubey@users.noreply.github.com>
2019-01-26 18:00:19 +0530

    temp commit

--- assignment1.py
+++ assignment1.py
@@ -1,17 +1,12 @@
 x=list(input("enter the master word "))
 r=list()
 r.insert(0,"".join(y for y in x))
-#for i in x:
-#    print(i)
 n=len(x)
 for k in range(1,n):
     c=0
-    #print("                           the size of empty is{}".format(k))
     for j in range(0,n-k+1):
-        #print("the {}th iteration from {} to {}".format(j,j,n-k))
         temp=x.copy()
         for p in range(j,j+k):
-            #print("removing from {} to {} location now on {}".format(j,j+k,p))
             temp[p]='?'
             c+=1
         for v in range(c):
@@ -19,6 +14,6 @@ for k in range(1,n):
         c=0
         finalstring="".join(b for b in temp)
         r.insert(len(r),finalstring)
-#r.sort()
+r.sort()
 print(r)
 print("total such possible combinations are {}".format(len(r)))
